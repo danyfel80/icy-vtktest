0db998b472450cece58052a3502514d0a086182d
Daniel Felipe Gonzalez Obando <danyfel80@gmail.com>
2016-03-31 12:21:50 +0200

    Initial commit

---
+++ src/plugins/danyfel80/vtktest/VTKTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package plugins.danyfel80.vtktest;
+
+import plugins.adufour.ezplug.EzPlug;
+import plugins.adufour.ezplug.EzVarSequence;
+
+/**
+ * VTK test class that uses an overlay to show 3d lines
+ * @author Daniel Felipe Gonzalez Obando
+ */
+public class VTKTest extends EzPlug {
+
+  private EzVarSequence inSequence;
+  /* (non-Javadoc)
+   * @see plugins.adufour.ezplug.EzPlug#initialize()
+   */
+  @Override
+  protected void initialize() {
+    inSequence = new EzVarSequence("Sequence");
+    addEzComponent(inSequence);
+  }
+  
+  /* (non-Javadoc)
+   * @see plugins.adufour.ezplug.EzPlug#execute()
+   */
+  @Override
+  protected void execute() {
+    if (inSequence.getValue() != null) {
+      inSequence.getValue().addOverlay(new TestOverlay("Lines"));
+    }
+  }
+
+  /* (non-Javadoc)
+   * @see plugins.adufour.ezplug.EzPlug#clean()
+   */
+  @Override
+  public void clean() {}
+}
