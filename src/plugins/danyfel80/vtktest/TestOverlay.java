f37fa6ca6a0ae43cb98cd7d30170dd71b0e49d8f
Daniel Felipe Gonzalez Obando <danyfel80@gmail.com>
2016-03-31 17:26:40 +0200

    NO CHANGE

--- src/plugins/danyfel80/vtktest/TestOverlay.java
+++ src/plugins/danyfel80/vtktest/TestOverlay.java
@@ -19,60 +19,60 @@ public class TestOverlay extends Overlay implements VtkPainter {
 
   private static final double[][] cube_vertices = new double[][] { {-10, -10, -10}, {-10, 10, -10}, {10, 10, -10},
     {10, -10, -10}, {-10, -10, 10}, {-10, 10, 10}, {10, 10, 10}, {10, -10, 10}};
-    
-  private vtkActor linesActor;
-  
-  
-  public TestOverlay(String name) {
-    super(name);
-    init();
-  }
 
-  private void init() {
-    // vertex data and fast java data conversion for vertices
-    final vtkPoints points = VtkUtil.getPoints(cube_vertices);
-    
-    /*
+    private vtkActor linesActor;
+
+
+    public TestOverlay(String name) {
+      super(name);
+      init();
+    }
+
+    private void init() {
+      // vertex data and fast java data conversion for vertices
+      final vtkPoints points = VtkUtil.getPoints(cube_vertices);
+
+      /*
     // associate points to line sequence
     final vtkPolyLine lines = new vtkPolyLine();
     lines.GetPointIds().SetNumberOfIds(cube_vertices.length);
     for (int v = 0; v < cube_vertices.length; v++) {
       lines.GetPointIds().SetId(v, v);
     }
-    */
-    // Create a cell array to store the lines in and add the lines to it
-    vtkCellArray lines = new vtkCellArray();
-    
-    
-    for (int v = 0; v < cube_vertices.length-2; v++) {
-      vtkLine line = new vtkLine();
-      line.GetPointIds().SetId(0, v);
-      line.GetPointIds().SetId(1, v+1);
-      lines.InsertNextCell(line);
+       */
+      // Create a cell array to store the lines in and add the lines to it
+      vtkCellArray lines = new vtkCellArray();
+
+
+      for (int v = 0; v < cube_vertices.length-2; v++) {
+        vtkLine line = new vtkLine();
+        line.GetPointIds().SetId(0, v);
+        line.GetPointIds().SetId(1, v+1);
+        lines.InsertNextCell(line);
+      }
+
+      // Create poly data
+      final vtkPolyData polyData = new vtkPolyData();
+      // set vertex to the poly
+      polyData.SetPoints(points);
+      // set lines to the poly
+      polyData.SetLines(lines);
+
+
+      // add actor to the renderer
+      final vtkPolyDataMapper polyMapper = new vtkPolyDataMapper();
+      polyMapper.SetInputData(polyData);
+
+      linesActor = new vtkActor();
+      linesActor.SetMapper(polyMapper);
     }
-    
-    // Create poly data
-    final vtkPolyData polyData = new vtkPolyData();
-    // set vertex to the poly
-    polyData.SetPoints(points);
-    // set lines to the poly
-    polyData.SetLines(lines);
-    
 
-    // add actor to the renderer
-    final vtkPolyDataMapper polyMapper = new vtkPolyDataMapper();
-    polyMapper.SetInputData(polyData);
-
-    linesActor = new vtkActor();
-    linesActor.SetMapper(polyMapper);
-  }
-
-  /* (non-Javadoc)
-   * @see icy.painter.VtkPainter#getProps()
-   */
-  @Override
-  public vtkProp[] getProps() {
-    return new vtkProp[] {linesActor};
-  }
+    /* (non-Javadoc)
+     * @see icy.painter.VtkPainter#getProps()
+     */
+    @Override
+    public vtkProp[] getProps() {
+      return new vtkProp[] {linesActor};
+    }
 
 }
